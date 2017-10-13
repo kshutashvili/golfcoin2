abef35c23e7d1f0273c071abfca62f9a41af7a89
antons <anton.shestakov2@gmail.com>
2017-10-13 16:23:25 +0300

    content type for document download

--- users/views.py
+++ users/views.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import os
+import magic
+from django.conf import settings
 from django.http.response import HttpResponse, HttpResponseForbidden
 from django.utils.translation import ugettext as _
 from django.contrib.auth import get_user_model
 from django.db.models import Q
-
 
 User = get_user_model()
 
@@ -24,7 +25,12 @@ def document_view(request, filename):
 
     if document_obj and (document_obj == request.user or request.user.is_superuser):
         response = HttpResponse()
-        response['X-Accel-Redirect'] = os.path.join('/media/', filename)
+        url = os.path.join('/media/', filename)
+        path = os.path.join(settings.MEDIA_ROOT, filename)
+        mime = magic.Magic(mime=True)
+        cont_type = mime.from_file(path)
+        response['Content-Type'] = cont_type
+        response['X-Accel-Redirect'] = url
         return response
     else:
         return HttpResponseForbidden(_("Restricted Access"))
